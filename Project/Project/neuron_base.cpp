ab9af73658306d6aba0b69dfe1cc07e022bd3702
TheMusicBoy <TheMusicBoy666@mail.ru>
2022-02-26 11:21:27 +1000

    ineuron_base, oneuron_base, mneuron_base

--- Project/Project/neuron_base.cpp
+++ Project/Project/neuron_base.cpp
@@ -8,4 +8,56 @@ namespace n {
 		return _value;
 	}
 
+	//Synaps
+
+	Synaps::Synaps(ineuron_ptr input_ptr, oneuron_ptr output_ptr) : input(input_ptr), output(output_ptr) {
+		input->addOutput(Synaps_ptr(this));
+		output->addInput(Synaps_ptr(this));
+	}
+
+	// Synaps_ptr
+
+	Synaps_ptr::Synaps_ptr(Synaps* ptr) : synaps(ptr) {}
+	
+	Synaps_ptr::~Synaps_ptr() { // доделать
+		delete synaps;
+	}
+
+	inline Synaps& Synaps_ptr::operator*() {
+		return *synaps;
+	}
+
+	// Input Neuron
+
+	ineuron_base::ineuron_base() : neuron_base() {}
+	
+	ineuron_base::~ineuron_base() {
+		output_neurons.clear();
+	}
+
+	void ineuron_base::addOutput(Synaps_ptr output_neuron) {
+		output_neurons.push_back(output_neuron);
+	}
+
+	// Output Neuron
+
+	oneuron_base::oneuron_base() : neuron_base() {}
+
+	oneuron_base::~oneuron_base() {
+		input_neurons.clear();
+	}
+
+	void oneuron_base::addInput(Synaps_ptr input_neuron) {
+		input_neurons.push_back(input_neuron);
+	}
+
+	// Middle Neuron
+
+	mneuron_base::mneuron_base() : neuron_base() {}
+
+	mneuron_base::~mneuron_base() {
+		input_neurons.clear();
+		output_neurons.clear();
+	}
+
 }
--- Project/Project/neuron_base.h
+++ Project/Project/neuron_base.h
@@ -5,27 +5,81 @@
 #include <vcruntime.h>
 #endif
 
+#include <list>
+
 namespace n {
 
 	using _value_t		= float;
 	using act_code_t	= size_t;
+	using weight		= float;
 
 	class neuron_base {
 	public:
 		neuron_base();
-		virtual ~neuron_base() = NULL;
 
 		virtual _value_t activate(act_code_t) = NULL;
 
-		inline _value_t getVal();
+		virtual inline _value_t getVal();
 
 	protected:
 		act_code_t act_code;
 		_value_t _value;
 	};
 
-	using _Nodeptr = neuron_base*;
+	using neuron_ptr = neuron_base*;
 
+	struct Synaps;
+
+	struct Synaps_ptr {
+		Synaps_ptr(Synaps* ptr);
+		~Synaps_ptr();
+
+		inline Synaps& operator*();
+		
+		Synaps* synaps;
+	};
+
+	class ineuron_base : virtual public neuron_base {
+	public:
+		ineuron_base();
+		virtual ~ineuron_base();
+
+		virtual void addOutput(Synaps_ptr output_neuron);
+
+	protected:
+		
+		std::list<Synaps_ptr> output_neurons;
+
+	};
+
+	using ineuron_ptr = ineuron_base*;
+
+	class oneuron_base : virtual public neuron_base {
+	public:
+		oneuron_base();
+		virtual ~oneuron_base() = NULL;
+
+		virtual void addInput(Synaps_ptr input_neuron);
+
+	protected:
+
+		std::list<Synaps_ptr> input_neurons;
+	};
+
+	using oneuron_ptr = oneuron_base*;
+
+	struct Synaps {
+		Synaps(ineuron_ptr input_ptr, oneuron_ptr output_ptr);
+		ineuron_ptr input;
+		oneuron_ptr output;
+		weight weight;
+	};
+
+	class mneuron_base : virtual public ineuron_base, virtual public oneuron_base {
+	public:
+		mneuron_base();
+		~mneuron_base();
+	};
 }
 
 #endif
